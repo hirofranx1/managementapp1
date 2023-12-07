d4b592e7923096b15ae689a5dbab9cb0917e1e44
Gregg <gregg.marayan@gmail.com>
2023-12-07 20:34:37 +0800

    added functionality to admin login page

--- server/server.js
+++ server/server.js
@@ -44,6 +44,34 @@ app.get('/getUsers',(req, res)=>{
     });
 })
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+  
+    db.query(
+      'SELECT admin_password FROM admin_cred WHERE admin_username = ?',
+      [email],
+      (err, data) => {
+        if (err) {
+          console.error('Error executing query:', err);
+          res.status(500).json({ error: 'Error executing query' });
+        } else {
+          if (data.length > 0) {
+            const storedPassword = data[0].admin_password;
+            if (storedPassword === password) {
+              // Passwords match, login successful
+              res.json({ message: 'Login successful' });
+            } else {
+              // Passwords don't match, login failed
+              res.status(401).json({ error: 'Invalid email or password' });
+            }
+          } else {
+            // No user found with the given email
+            res.status(404).json({ error: 'User not found' });
+          }
+        }
+      }
+    );
+  });  
 
 
 
