40a3e4eb0d977f1f9311a7818b6f583833cc029a
Gregg <gregg.marayan@gmail.com>
2023-12-08 18:07:28 +0800

    UI DONE for management app

--- ManagementApp/src/Dashboard.jsx
+++ ManagementApp/src/Dashboard.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import logo from './assets/tigum_logos/logo-big.png';
+
 
 import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
-  
+
 
   const [userData, setUserData] = useState([{}]);
   const [userCount, setUserCount] = useState(0);
   const [newUsers, setNewUsers] = useState(0);
   const [dailyUsers, setDailyUsers] = useState(0);
-  
+
   const history = useNavigate();
 
   useEffect(() => {
@@ -24,7 +26,7 @@ function Dashboard() {
         console.log(error);
       });
   }
-  , []);
+    , []);
   useEffect(() => {
     axios.get('http://localhost:8000/newUsers')
       .then((response) => {
@@ -35,7 +37,7 @@ function Dashboard() {
         console.log(error);
       });
   }
-  , []);
+    , []);
 
   useEffect(() => {
     axios.get('http://localhost:8000/dailyUsers')
@@ -46,13 +48,12 @@ function Dashboard() {
       .catch((error) => {
         console.log(error);
       });
-  } , []);
-  
-
-  
+  }, []);
 
 
-  // Logout function (you can implement the actual logout logic here)
+
+
+
   const handleLogout = () => {
     history('/');
     console.log('Logout clicked');
@@ -62,44 +63,68 @@ function Dashboard() {
     console.log('Users clicked');
   };
 
+  const gotoDashboard = () => {
+    history('/dashboard');
+    console.log('Users clicked');
+  };
+
   return (
     <>
       {/* Navigation Side Bar */}
-      <div>
-        <div className="logo">
-          <div className='text-center'>
-            <h1>LOGO Here</h1>
-            <p>Admin Area</p>
+      <div className="d-flex" style={{ minHeight: "100vh" }}>
+        {/* Sidebar */}
+        <div className="bg-dark text-light d-flex flex-column justify-content-between p-4" style={{ width: "250px", minHeight: "100vh" }}>
+          <div>
+            <div className="logo text-center mb-4">
+              <img src={logo} style={{ width: '150px' }} />
+              <p>Admin Area</p>
+            </div>
+            <div className="navigation-menu">
+              {/* Add your navigation menu items here */}
+              <ul className="list-group">
+                <li className="list-group-item mb-2 btn" onClick={gotoUsers}>Users</li>
+                <li className="list-group-item mb-2 btn" onClick={gotoDashboard}>Dashboard</li>
+                {/* Add more menu items as needed */}
+              </ul>
+            </div>
           </div>
-          {/* Navigation Options */}
+          {/* Logout Button */}
+          <button className="btn btn-primary" onClick={handleLogout}>
+            Logout
+          </button>
         </div>
-        <div className="navigation-menu">
-          {/* Add your navigation menu items here */}
-        </div>
-        {/* Logout Button */}
-        <button className="btn btn-primary" onClick={handleLogout}>
-          Logout
-        </button>
-      </div>
-      <div className="container-fluid bg-white mt-4">
-          <button className='btn btn-primary' onClick={gotoUsers} >Users</button>
-      </div>
-        {/* Dashboard */}
-      <div className="dashboard">
-        <h3> DASHBOARD</h3>
-        {/* Dashboard Cards Static */}
-        <div className="cards">
-          <div className="card">
-            <h2>Total User Count</h2>
-            <p>{userCount}</p>
-          </div>
-          <div className="card">
-            <h2>New Monthly Users</h2>
-            <p>{newUsers}</p>
-          </div>
-          <div className="card">
-            <h2>Daily Active User</h2>
-            <p>{dailyUsers}</p>
+
+        {/* Main content */}
+        <div className="container-fluid bg-white mt-4">
+          <div className="container mt-4">
+            <h1 className="text-center mb-4">Dashboard</h1>
+
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+              <div className="col">
+                <div className="card h-100">
+                  <div className="card-body">
+                    <h5 className="card-title">Total User Count</h5>
+                    <p className="card-text fs-1 text-center">{userCount}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card h-100">
+                  <div className="card-body">
+                    <h5 className="card-title">New Monthly Users</h5>
+                    <p className="card-text fs-1 text-center">{newUsers}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card h-100">
+                  <div className="card-body">
+                    <h5 className="card-title">Daily Active Users</h5>
+                    <p className="card-text fs-1 text-center">{dailyUsers}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- ManagementApp/src/ManagementApp.jsx
+++ ManagementApp/src/ManagementApp.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import logo from './assets/tigum_logos/logo-big.png';
 
 const YourComponent = () => {
   const [userData, setUserData] = useState([{}]);
- 
+
   const [user, setUser] = useState({});
   const [showDelete, setShowDelete] = useState(false);
   const [userId, setUserId] = useState('');
@@ -23,7 +24,7 @@ const YourComponent = () => {
       });
   }, []);
   async function deleteUser() {
-     axios.put('http://localhost:8000/deleteUser', {
+    axios.put('http://localhost:8000/deleteUser', {
       id: userId,
     }).then((response) => {
       console.log(response);
@@ -41,99 +42,127 @@ const YourComponent = () => {
   };
 
   console.log(userData);
- 
 
 
 
-
-  const goback = () => {
-    history('/dashboard');
+  const handleLogout = () => {
+    history('/');
     console.log('Logout clicked');
+  };
+
+  const gotoUsers = () => {
+    history('/managementApp');
+    console.log('Users clicked');
+  };
+
+  const gotoDashboard = () => {
+    history('/dashboard');
+    console.log('Users clicked');
   };
 
 
   return (
     <>
+      {/* Navigation Side Bar */}
+      <div className="d-flex" style={{ minHeight: "100vh" }}>
+        {/* Sidebar */}
+        <div className="bg-dark text-light d-flex flex-column justify-content-between p-4" style={{ width: "250px", minHeight: "100vh" }}>
+          <div>
+            <div className="logo text-center mb-4">
+              <img src={logo} style={{ width: '150px' }} />
+              <p>Admin Area</p>
+            </div>
+            <div className="navigation-menu">
+              {/* Add your navigation menu items here */}
+              <ul className="list-group">
+                <li className="list-group-item mb-2 btn" onClick={gotoUsers}>Users</li>
+                <li className="list-group-item mb-2 btn" onClick={gotoDashboard}>Dashboard</li>
+                {/* Add more menu items as needed */}
+              </ul>
+            </div>
+          </div>
+          {/* Logout Button */}
+          <button className="btn btn-primary" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
 
-      {/* header blue card */}
 
-      <div className="container-fluid bg-white mt-4">
-        <div className="container px-0">
-          <nav className="navbar bg-info border rounded-4 d-flex justify-content-between align-items-center m-0 px-3">
-            <p className="display-6 mb-0">Welcome, Admin</p>
-            <button className='btn btn-light' onClick={goback}>Go Back</button>
-          </nav>
+
+        {/* Main content */}
+        <div className="d-flex flex-column pt-4" style={{ width: "100%" }}>
+          {/* Header blue card */}
+          <div className="container-fluid rounded-4 mb-4">
+            <div className="container px-0">
+              <nav className="navbar bg-primary border rounded-4 d-flex justify-content-between align-items-center m-0 px-3">
+                <p className="display-6 mb-0 text-light">Welcome, Admin</p>
+              </nav>
+            </div>
+          </div>
+          {/* End of header blue card */}
+
+          {/* table of users */}
+          <div className="container-fluid bg-white py-4">
+            <div className="container">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope='col'>First Name</th>
+                    <th scope='col'>Last Name</th>
+                    <th scope='col'>Email</th>
+                    <th scope='col'>Latest Activity</th>
+                    <th scope='col'>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {userData.map((user, index) => (
+
+                    <tr key={index}>
+                      <td>{user.firstname}</td>
+                      <td>{user.lastname}</td>
+                      <td>{user.email}</td>
+                      <td>{new Date(user.latest_activity).toLocaleDateString()}</td>
+                      <td>
+                        {isActivityOld(user.latest_activity) ? (
+                          <button
+                            onClick={() => {
+                              setShowDelete(true);
+                              setUser(user);
+                              setUserId(user.user_id);
+                            }}
+                            className="btn btn-danger"
+                          >
+                            Delete User
+                          </button>
+                        ) : "Cannot Delete User"}
+                      </td>
+
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          {/* end of table of users */}
         </div>
       </div>
 
-      {/* end of header blue card */}
-
-
-      {/* table of users */}
-
-      <div className="container-fluid bg-white mt-4 py-4">
-        <div className="container">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope='col'>First Name</th>
-                <th scope='col'>Last Name</th>
-                <th scope='col'>Email</th>
-                <th scope='col'>Latest Activity</th>
-                <th scope='col'>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {userData.map((user, index) => (
-
-                <tr key={index}>
-                  <td>{user.firstname}</td>
-                  <td>{user.lastname}</td>
-                  <td>{user.email}</td>
-                  <td>{new Date(user.latest_activity).toLocaleDateString()}</td>
-                  <td> 
-                    {isActivityOld(user.latest_activity) ? (
-                  <button
-                    onClick={() => {
-                      setShowDelete(true);
-                      setUser(user);
-                      setUserId(user.user_id);
-                    }}
-                    className="btn btn-danger"
-                  >
-                    Delete User
-                  </button>
-                ): "Cannot Delete User"}
-                </td>
-
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-
-      {/* end of table of users */}
       {showDelete && (
-          <Modal show={true} onHide={() => setShowDelete(false)} backdrop={false}>
-            <Modal.Header>
-              <Modal.Title>Delete User</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <p>Are you sure you want to delete this user?</p>
-            </Modal.Body>
-            <Modal.Footer>
-            <button className='btn btn-danger' onClick={() => {setShowDelete(false); deleteUser()}}>Delete</button>
-              <button className='btn btn-secondary' onClick={() => setShowDelete(false)}>Cancel</button>
-              
-            </Modal.Footer>
+        <Modal show={true} onHide={() => setShowDelete(false)} backdrop={false}>
+          <Modal.Header>
+            <Modal.Title>Delete User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Are you sure you want to delete this user?</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className='btn btn-danger' onClick={() => { setShowDelete(false); deleteUser() }}>Delete</button>
+            <button className='btn btn-secondary' onClick={() => setShowDelete(false)}>Cancel</button>
 
-          </Modal>
+          </Modal.Footer>
+
+        </Modal>
       )}
-
-
-
-
     </>
   );
 };
