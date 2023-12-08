4f664fe15fdf9f69c3049a26cdca36d1e2790e77
saldestella <stellaasalde@gmail.com>
2023-12-08 23:28:40 +0800

    Update Dashboard.jsx

--- ManagementApp/src/Dashboard.jsx
+++ ManagementApp/src/Dashboard.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import logo from './assets/tigum_logos/logo-big.png';
-
-
+import logo from './assets/white_tigum_logos/1whiteLog.svg';
 import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
-
-
-  const [userData, setUserData] = useState([{}]);
+  const [userData, setUserData] = useState([]);
   const [userCount, setUserCount] = useState(0);
   const [newUsers, setNewUsers] = useState(0);
   const [dailyUsers, setDailyUsers] = useState(0);
@@ -20,44 +16,38 @@ function Dashboard() {
       .then((response) => {
         setUserData(response.data);
         setUserCount(response.data.length);
-        console.log(response.data.length)
+        console.log(response.data.length);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
-  }
-    , []);
+  }, []);
+
   useEffect(() => {
     axios.get('http://localhost:8000/newUsers')
       .then((response) => {
-        console.log(response.data.result.length);
         setNewUsers(response.data.result.length);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
-  }
-    , []);
+  }, []);
 
   useEffect(() => {
     axios.get('http://localhost:8000/dailyUsers')
       .then((response) => {
-        console.log(response.data.result.length);
         setDailyUsers(response.data.result.length);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   }, []);
-
-
-
-
 
   const handleLogout = () => {
     history('/');
     console.log('Logout clicked');
   };
+
   const gotoUsers = () => {
     history('/managementApp');
     console.log('Users clicked');
@@ -65,71 +55,71 @@ function Dashboard() {
 
   const gotoDashboard = () => {
     history('/dashboard');
-    console.log('Users clicked');
+    console.log('Dashboard clicked');
+  };
+
+  const logoStyles = {
+    width: '150px',
+    boxShadow: '0 4px 4px rgba(0, 0, 0, 0.25)',
   };
 
   return (
-    <>
-      {/* Navigation Side Bar */}
-      <div className="d-flex" style={{ minHeight: "100vh"}}>
-        {/* Sidebar */}
-        <div className="text-light d-flex flex-column justify-content-between p-4" style={{ width: "250px", minHeight: "100vh", backgroundColor: "rgb(162, 188, 210)" }}>
-          <div>
-            <div className="logo text-center mb-4">
-              <img src={logo} style={{ width: '150px' }} />
-              <p>Admin Area</p>
-            </div>
-            <div className="navigation-menu">
-              {/* Add your navigation menu items here */}
-              <ul className="list-group">
-                <li className="list-group-item mb-2 btn" onClick={gotoUsers}>Users</li>
-                <li className="list-group-item mb-2 btn" onClick={gotoDashboard}>Dashboard</li>
-                {/* Add more menu items as needed */}
-              </ul>
-            </div>
+    <div className="d-flex" style={{ minHeight: '100vh', backgroundColor: 'rgb(162, 188, 210)', fontFamily: 'Inter, sans-serif' }}>
+      <div className="text-light d-flex flex-column justify-content-between p-4" style={{ width: '250px', minHeight: '100vh' }}>
+        <div>
+          <div className="logo text-center mb-4">
+            <img src={logo} style={logoStyles} alt="Logo" />
+            <p>Admin Area</p>
           </div>
-          {/* Logout Button */}
-          <button className="btn btn-primary" onClick={handleLogout}>
-            Logout
-          </button>
+          <div className="navigation-menu">
+            <ul className="list-group">
+              <li className="list-group-item mb-2 btn" onClick={gotoUsers}>
+                Users
+              </li>
+              <li className="list-group-item mb-2 btn" onClick={gotoDashboard}>
+                Dashboard
+              </li>
+            </ul>
+          </div>
         </div>
+        <button className="btn btn-primary" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
 
-        {/* Main content */}
-        <div className="container-fluid bg-white mt-4">
-          <div className="container mt-4">
-            <h1 className="text-center mb-4">Dashboard</h1>
+      <div className="container-fluid bg-white mt-4">
+        <div className="container mt-4">
+          <h1 className="text-center mb-4">Dashboard</h1>
 
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              <div className="col">
-                <div className="card h-100">
-                  <div className="card-body">
-                    <h5 className="card-title">Total User Count</h5>
-                    <p className="card-text fs-1 text-center">{userCount}</p>
-                  </div>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">Total User Count</h5>
+                  <p className="card-text fs-1 text-center">{userCount}</p>
                 </div>
               </div>
-              <div className="col">
-                <div className="card h-100">
-                  <div className="card-body">
-                    <h5 className="card-title">New Monthly Users</h5>
-                    <p className="card-text fs-1 text-center">{newUsers}</p>
-                  </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">New Monthly Users</h5>
+                  <p className="card-text fs-1 text-center">{newUsers}</p>
                 </div>
               </div>
-              <div className="col">
-                <div className="card h-100">
-                  <div className="card-body">
-                    <h5 className="card-title">Daily Active Users</h5>
-                    <p className="card-text fs-1 text-center">{dailyUsers}</p>
-                  </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">Daily Active Users</h5>
+                  <p className="card-text fs-1 text-center">{dailyUsers}</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-    </>
+    </div>
   );
 }
 
